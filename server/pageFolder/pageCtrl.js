708a34349f4820a068e0dddecb0e790a59239b78
BillyTheArtist <billy.iv.art@gmail.com>
2022-11-16 14:51:35 -0800

    another bug fix

--- server/pageFolder/pageCtrl.js
+++ server/pageFolder/pageCtrl.js
@@ -3,6 +3,6 @@ let path = require("path")
 module.exports = {
     home: (req, res) => {
         res.sendFile(path.join(__dirname, "../../client/index.html"))
-        res.sendFile(path.join(__dirname, "../../client/style.css"))
+        res.sendFile(path.join(__dirname, "../../client/styles.css"))
     }
 }
